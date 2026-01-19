7a6297a4e096ec40ccda50948e5506ad593c739c
Kanyin Olagbegi <kanyin.olagbegi@gmail.com>
2026-01-19 17:29:39 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "./", // Must be "/" for custom domain
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  server: {
-    host: "::",
-    port: 8080,
-    hmr: {
-      overlay: false,
-    },
-  },
-});
+  base: '/website1-main/', // ← MUST match your repo name
+})
+
