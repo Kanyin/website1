0ef2c1c9a3051211e01ff288e103b0cf3b647b14
Kanyin Olagbegi <kanyin.olagbegi@gmail.com>
2026-01-19 20:22:42 -0500

    Update vite.config.ts for custom domain
    Updating this file to allow custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -9,5 +9,5 @@ export default defineConfig({
       '@': path.resolve(__dirname, 'src'),
     },
   },
-  base: '/website1/', // Make sure this matches your repo name
+  base: '/', // Make sure this matches your repo name
 })
