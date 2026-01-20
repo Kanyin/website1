74da3f3826a7c1a0cb76ccc1e641fdd663968f7e
Kanyin Olagbegi <kanyin.olagbegi@gmail.com>
2026-01-20 15:50:54 -0500

    Remove Client Page from App
    To Restore add back:
    import Clients from "./pages/Clients";
    
    to the Route path files
    
              <Route path="/clients" element={<Clients />} />

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import { HashRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import About from "./pages/About";
 import Services from "./pages/Services";
-import Clients from "./pages/Clients";
+//import Clients from "./pages/Clients";
 import Contact from "./pages/Contact";
 import NotFound from "./pages/NotFound";
 
@@ -24,7 +24,6 @@ const App = () => (
           <Route path="/" element={<Index />} />
           <Route path="/about" element={<About />} />
           <Route path="/services" element={<Services />} />
-          <Route path="/clients" element={<Clients />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
