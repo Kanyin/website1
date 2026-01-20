38cc1564dfb0c68fe759b0b4d482fbf50f438fd0
Kanyin Olagbegi <kanyin.olagbegi@gmail.com>
2026-01-20 18:15:59 -0500

    Update Index.tsx Change BandReel to Video Carousel

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -3,7 +3,7 @@ import Hero from "@/components/home/Hero";
 import Introduction from "@/components/home/Introduction";
 import ServiceHighlights from "@/components/home/ServiceHighlights";
 // import Testimonials from "@/components/home/Testimonials";
-import BandReel from "@/components/home/BandReel";
+import VideoCarousel from "@/components/home/VideoCarousel";
 import CallToAction from "@/components/home/CallToAction";
 
 const Index = () => {
@@ -15,7 +15,7 @@ const Index = () => {
       {/*
       <Testimonials />
       */}
-      <BandReel />
+      <VideoCarousel />
       <CallToAction />
     </Layout>
   );
